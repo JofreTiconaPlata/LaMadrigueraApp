8cf74b31b84b513e1a70b9c8959e9e4d346c50eb
JOFRE TICONA PLATA <202202581@est.umss.edu>
2026-05-15 18:48:23 -0400

    feat(auth): agregar repositorio y casos de uso flutter

--- lib/features/auth/domain/usecases/login_usecase.dart
+++ lib/features/auth/domain/usecases/login_usecase.dart
@@ -1,1 +1,18 @@
+import 'package:la_madriguera/features/auth/domain/repositories/auth_repository.dart';
+import 'package:la_madriguera/shared/models/usuario_model.dart';
 
+class LoginUseCase {
+  const LoginUseCase(this._repository);
+
+  final AuthRepository _repository;
+
+  Future<UsuarioModel> call({
+    required String email,
+    required String password,
+  }) {
+    return _repository.login(
+      email: email,
+      password: password,
+    );
+  }
+}
