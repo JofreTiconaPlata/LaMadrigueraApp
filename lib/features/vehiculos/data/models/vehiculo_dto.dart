34b943d21856965c217398a232719e56b6c8f4bc
Jofre Ticona Plata <202202581@est.umss.edu>
2026-05-21 11:49:09 -0400

    feat(flutter): conectar datasource de vehiculos

--- lib/features/vehiculos/data/models/vehiculo_dto.dart
+++ lib/features/vehiculos/data/models/vehiculo_dto.dart
@@ -1,1 +1,31 @@
+class VehiculoDto {
+  const VehiculoDto({
+    required this.id,
+    required this.clienteId,
+    required this.placa,
+    required this.tipo,
+    required this.marca,
+    required this.modelo,
+    required this.color,
+  });
 
+  final int id;
+  final int clienteId;
+  final String placa;
+  final String tipo;
+  final String? marca;
+  final String? modelo;
+  final String? color;
+
+  factory VehiculoDto.fromJson(Map<String, dynamic> json) {
+    return VehiculoDto(
+      id: json['id'] as int,
+      clienteId: json['clienteId'] as int,
+      placa: json['placa'] as String,
+      tipo: json['tipo'] as String,
+      marca: json['marca'] as String?,
+      modelo: json['modelo'] as String?,
+      color: json['color'] as String?,
+    );
+  }
+}
