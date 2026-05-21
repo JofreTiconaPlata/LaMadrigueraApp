1b18db7ef6e78ab9a835776d9d02b7c5722fc062
Jofre Ticona Plata <202202581@est.umss.edu>
2026-05-21 11:36:56 -0400

    feat(flutter): conectar datasource de parqueos

--- lib/features/parqueos/data/datasources/parqueos_remote_datasource.dart
+++ lib/features/parqueos/data/datasources/parqueos_remote_datasource.dart
@@ -1,1 +1,32 @@
+import 'package:dio/dio.dart';
+import 'package:la_madriguera/core/network/api_endpoints.dart';
+import 'package:la_madriguera/core/network/dio_client.dart';
+import 'package:la_madriguera/features/parqueos/data/models/parqueo_dto.dart';
 
+class ParqueosRemoteDataSource {
+  ParqueosRemoteDataSource({Dio? dio}) : _dio = dio ?? DioClient.instance;
+
+  final Dio _dio;
+
+  Future<List<ParqueoDto>> getParqueos() async {
+    final response = await _dio.get<Map<String, dynamic>>(
+      ApiEndpoints.parqueos,
+    );
+
+    final data = response.data?['data'] as List<dynamic>? ?? [];
+
+    return data
+        .map((item) => ParqueoDto.fromJson(item as Map<String, dynamic>))
+        .toList();
+  }
+
+  Future<ParqueoDto> getParqueoById(int id) async {
+    final response = await _dio.get<Map<String, dynamic>>(
+      '${ApiEndpoints.parqueos}/$id',
+    );
+
+    final data = response.data?['data'] as Map<String, dynamic>;
+
+    return ParqueoDto.fromJson(data);
+  }
+}
